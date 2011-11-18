c242d6708a899e60c908ffeaabceaa845be6781f
unknown <usr@.(none)>
2011-11-18 15:09:53 -0600

    added tests for page titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def login
+    @title = "Login"
   end
 
   def orders
+    @title = "Orders"
   end
 
   def customize
+    @title = "Customize"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,12 +7,20 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Home")
+    end
   end
 
   describe "GET 'login'" do
     it "returns http success" do
       get 'login'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'login'
+      response.should have_selector("title", :content => "Login")
     end
   end
 
@@ -21,12 +29,20 @@ describe PagesController do
       get 'orders'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'orders'
+      response.should have_selector("title", :content => "Orders")
+    end
   end
 
   describe "GET 'customize'" do
     it "returns http success" do
       get 'customize'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'customize'
+      response.should have_selector("title", :content => "Customize")
     end
   end
 
