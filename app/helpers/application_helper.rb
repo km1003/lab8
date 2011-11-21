f285913ff33deb44170f2a5a8c2a16ed641eec99
unknown <guy@.(none)>
2011-11-20 18:47:18 -0600

    added page content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def pc1
     image_tag("custom_pc1.jpg", :alt => "lab8", :class => "pic")
   end
+  def pc1t
+    image_tag("custom_pc1t.jpg", :alt => "lab8", :class => "pic")
+  end
 end
